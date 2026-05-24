50f1c569936cee437a8e5556be38484c22575bf6
Jesus <jesustoledomx@gmail.com>
2026-05-24 16:23:45 -0600

    Today make exersices with learn javascript if, else if

---
+++ ejercicio3.js
@@ -0,0 +1,59 @@
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are not an adult");
+}
+
+let temperature = 15;
+if (temperature > 30) {
+    console.log("It's hot outside");
+} else {
+    console.log("It's cold outside");
+}
+
+let money = 100;
+if (money > 50) {
+    console.log("You have enough money to buy a new car");
+} else {
+    console.log("You don't have enough money to buy a new car");
+}
+
+let battery = 10;
+if (battery > 20) {
+    console.log("Your phone is charged");
+} else {
+    console.log("Your phone is not charged");
+}
+
+let internet = 80;
+if (internet >=50) {
+    console.log("You have good internet connection");
+} else {
+    console.log("You have bad internet connection");
+}
+
+let englishLevel = 60;
+if (englishLevel >=70) {
+    console.log("You are fluent in English");
+} else {
+    console.log("You are not fluent in English");
+}
+
+//Ejercicio A
+
+let weather = 35;
+if (weather >30) {
+    console.log("It's hot outside");
+} else {
+    console.log("It's a nice weather outside");
+}
+
+//Ejercicio B
+
+let javascriptLevel = 40;
+if (javascriptLevel >= 70) {
+    console.log("You are ready for React");
+} else {
+    console.log("Keep practicing JavaScript");
+}
---
+++ ejercicio4.js
@@ -0,0 +1,66 @@
+let score = 95;
+if (score = 90) {
+    console.log("Excellent");
+} else if (score = 70) {
+    console.log("You passed the test");
+}  else {
+    console.log("You failed the test");
+} 
+
+let temperature = 25;
+if (temperature > 30) {
+    console.log("It's hot outside");
+} else if (temperature > 20) {
+    console.log("It's warm outside");
+} else {
+    console.log("It's cold outside");
+}
+
+let hours = 10;
+if (hours >= 8) {
+    console.log("You are working full time at your job");
+} else if (hours >= 4) {
+    console.log("You are working part time at your job");
+} else {
+    console.log("You are not working at your job");
+}
+
+let englishLevel = 75;
+if (englishLevel= 90) {
+    console.log("You are fluent in English");
+} else if (englishLevel= 70) {
+    console.log("You are good in English");
+} else {
+    console.log("You are not fluent in English");
+}
+
+let internetSpeed = 120;
+if (internetSpeed >= 100) {
+    console.log("You have a fast internet connection");
+} else if (internetSpeed >= 50) {
+    console.log("You have a good internet connection");
+} else {
+    console.log("You have a slow internet connection");
+}
+
+//Ejercicio A
+
+let salary = 4000;
+if (salary >= 5000) {
+    console.log("You have a high salary");
+} else if (salary >= 3000) {
+    console.log("You have a normal salary");
+} else {
+    console.log("You have a low salary");
+}
+
+//Ejercicio B
+
+let age = 17;
+if (age >= 60) {
+    console.log("You are a senior");
+} else if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
