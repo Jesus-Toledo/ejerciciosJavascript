572448715006712166cf3acecbe1314b5e50dd74
Jesus <jesustoledomx@gmail.com>
2026-05-23 21:52:15 -0600

    Today working with exercise with sentence IF

---
+++ ejercicio2.js
@@ -0,0 +1,63 @@
+let age = 15;
+if (age >= 18) {
+    console.log("You are an adult");
+} 
+
+let temperature = 40;
+if (temperature >30) {
+    console.log("It's hot outside");
+}
+
+let score = 90;
+if (score >= 70) {
+    console.log("You passed the test");
+}
+
+let money = 100;
+if (money >50 ) {
+    console.log("You have enough money to buy a new car");
+}
+
+let hours = 9;
+if (hours >= 8) {
+    console.log("You are working full time at your job");
+}
+
+let yourName = "Jesús";
+if (yourName === "Jesús") {
+    console.log("You are wellcome");
+}
+
+let city = "Guadalajara";
+if (city === "Guadalajara") {
+    console.log("You live in a beautiful city");
+}
+
+let battery = 15;
+if (battery < 20) {
+    console.log("Your battery is low");
+}
+
+let speed = 120;
+if (speed > 100) {
+    console.log("You are going too fast");
+}
+
+let englishLevel = 80;
+if (englishLevel >= 70) {
+    console.log("You are fluent in English");
+}
+
+//Ejercicio A
+
+let salary = 2000;
+if (salary > 1500) {
+    console.log("You have a good salary");
+}
+
+//Ejercicio B
+
+let experience = 2;
+if (experience >= 1 ) {
+    console.log("You are a Junior Developer");
+}
