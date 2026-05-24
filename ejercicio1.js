d53f0509b5c11e00eac1b62efc7d1bba17a0054f
Jesus <jesustoledomx@gmail.com>
2026-05-23 21:18:14 -0600

    This is a file with Javascript excersice

---
+++ ejercicio1.js
@@ -0,0 +1,39 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+let age = 36;
+console.log(age + 1);
+
+let price = 100;
+console.log(price - 20);
+
+let hours = 8;
+let salaryperHours= 50;
+console.log(hours * salaryperHours);
+
+let myName = "Jesús";
+let lastName = "García";
+console.log(myName + " " + lastName);
+
+let city = "Guadalajara";
+console.log(`I live in ${city}`);
+
+let favoriteBand = "Coldplay";
+let favoriteSong = "In my place";
+console.log(`My favorite band is ${favoriteBand} and my favorite song is ${favoriteSong}`);
+
+//Ejercicio A
+let productPrice = 250;
+let discount = 50;
+console.log(productPrice - discount);
+
+//Ejercicio B
+let firstName = "Dayessi";
+let country = "México";
+let job = "Accountant";
+console.log(`${firstName} lives in ${country} and works as an  ${job}`);
---
+++ ejercicios.js
@@ -0,0 +1,35 @@
+
+// Ejercicio 1
+let country = "México"
+console.log(`Mi país de origen es ${country}`)
+
+// Ejercicio 2
+let favoriteFood = "pollo asado"
+console.log(`Mi comida favorita es ${favoriteFood}`)
+
+// Ejercicio 3
+let myAge = 38
+console.log(`Mi edad es ${myAge}`)
+
+// Ejercicio 4
+let myJob = "NOC engineer"
+console.log(`Mi trabajo es ${myJob}`)
+
+// Ejercicio 5
+let music = "Coldplay"
+console.log(`Mi grupo favorito es ${music}`)
+
+//ejercicio A
+let myName = "Jesús"
+let city = "Guadalajara"
+console.log(`Mi nombre es ${myName}`)
+console.log(`Mi ciudad es ${city}`)
+
+//ejercicio B
+let favoriteColor = "Verde pastel"
+let favoriteMovie = "El señor de los anillos"
+let favoriteNumber = 13
+
+console.log(`Mi color favorito es ${favoriteColor}`)
+console.log(`Mi película favorita es ${favoriteMovie}`)
+console.log(`Mi número favorito es ${favoriteNumber}`)
